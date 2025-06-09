36d469df9e652a42a3679e285d4258944d2c3d61
friday1one <friday1one@gmail.com>
2025-06-09 15:28:28 +0500

    Create auth-register.js

---
+++ auth-register.js
@@ -0,0 +1,13 @@
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Method Not Allowed',
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: 'Register endpoint hit!',
+  };
+};
